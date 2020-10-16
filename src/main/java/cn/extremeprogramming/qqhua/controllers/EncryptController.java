76664d2f7e006c4bdd0976905b14dbc9009c5a8c
huisheng.jin <huisheng.jinge@gmail.com>
2020-10-16 11:50:42 +0800

    加密增加入参参数

--- src/main/java/cn/extremeprogramming/qqhua/controllers/EncryptController.java
+++ src/main/java/cn/extremeprogramming/qqhua/controllers/EncryptController.java
@@ -2,6 +2,8 @@ package cn.extremeprogramming.qqhua.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author huisheng.jin
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class EncryptController {
     @PostMapping("/encrypt")
-    public String encrypt() {
+    public String encrypt(@RequestParam("message") String message,
+                          @RequestParam("picture") MultipartFile picture
+    ) {
         return "encrypted.html";
     }
 }
--- src/test/java/cn/extremeprogramming/qqhua/controllers/EncryptControllerTest.java
+++ src/test/java/cn/extremeprogramming/qqhua/controllers/EncryptControllerTest.java
@@ -3,7 +3,10 @@ package cn.extremeprogramming.qqhua.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.InputStream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -19,9 +22,21 @@ public class EncryptControllerTest {
     private MockMvc mvc;
 
     @Test
-    void should_render_encrypt_result_page_given_message_and_picture() throws Exception {
+    void should_throw_error_400_given_message_or_picture_post() throws Exception {
         mvc.perform(multipart("/encrypt"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void should_render_encrypt_result_page_given_message_and_picture() throws Exception {
+        InputStream resource = getClass().getClassLoader().getResourceAsStream("banner.png");
+        MockMultipartFile picture = new MockMultipartFile("picture", resource);
+
+        mvc.perform(multipart("/encrypt")
+                .file(picture)
+                .param("message", "Hello!"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith("text/html"));
     }
+
 }
