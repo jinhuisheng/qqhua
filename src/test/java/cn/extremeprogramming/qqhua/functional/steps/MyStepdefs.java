45db6a7ed32cf9f1b12df5069784f77dd109a03d
huisheng.jin <huisheng.jinge@gmail.com>
2020-10-24 11:04:32 +0800

    cucumber 改为使用java8 lambda 配置修改

--- src/test/java/cn/extremeprogramming/qqhua/functional/steps/MyStepdefs.java
+++ src/test/java/cn/extremeprogramming/qqhua/functional/steps/MyStepdefs.java
@@ -1,23 +1,18 @@
 package cn.extremeprogramming.qqhua.functional.steps;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java8.En;
 
 /**
  * @author huisheng.jin
  * @date 2020/10/24.
  */
-public class MyStepdefs {
-    @Given("user opens home page")
-    public void userOpensHomePage() {
-    }
-
-    @When("user uploads picture with message {string}")
-    public void userUploadsPictureWithMessage(String arg0) {
-    }
-
-    @Then("user sees encoded picture")
-    public void userSeesEncodedPicture() {
+public class MyStepdefs implements En {
+    public MyStepdefs() {
+        Given("^user opens home page$", () -> {
+        });
+        When("^user uploads picture with message \"([^\"]*)\"$", (String arg0) -> {
+        });
+        Then("^user sees encoded picture$", () -> {
+        });
     }
 }
